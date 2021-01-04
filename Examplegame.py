4fda521761c3f3c84de34cf73e0e519d31ea2e53
Riedler <riedler@gelse.net>
2021-01-04 11:25:25 +0100

    archival changes - last commit

--- Examplegame.py
+++ Examplegame.py
@@ -1,29 +1,49 @@
-import rgraphics
+import rgraphics as rgr
 import os
 import random
 import asyncio
-rgraphics.intro()
-screen=rgraphics.graphic()
-os.system("clear")
-fp=rgraphics.fpslimiter()
+fp=rgr.FpsLimiter()
+shds=rgr.Shades()
+colr=rgr.Color()
+Px=rgr.Px
+screen=rgr.Graphic(width=300,height=100,bg=Px(shade=shds.d))
 content=[]
-for shade in ["B","D","G","L","W"," "]:
+objlist=["","","",""]
+for shade in shds.shades(let=True,shds=False):
 	precontent=[]
-	for color in ["1","2","3","4","5","6","7","8"]:
-		precontent.append(color+shade+"0")
+	for color in colr.colrs():
+		precontent.append(Px(color,shade))
 	content.append(precontent)
-screen.init(38,83,"B1")
-ball=rgraphics.graphic()
-ball.content=rgraphics.dispform(content)
-ball.posx=random.randrange(5,75)
-ball.posy=random.randrange(5,30)
-ball.speedx=1
-ball.speedy=1
-while True:
-	fp.start()
-	asyncio.run(screen.clear("L"))
-	ball.move()
-	ball.stayin(screen,"bounce")
-	ball.draw(screen)
-	screen.display(colouring=True)
-	fp.end(60)
+objlist[1]=rgr.Graphic(content=content,posx=random.randrange(5,75),posy=random.randrange(5,30),speedx=1,speedy=1)
+content=[]
+for shade in shds.shades(let=False):
+	precontent=[]
+	for color in colr.colrs():
+		precontent.append(Px(color,shade))
+	content.append(precontent)
+objlist[0]=rgr.Graphic(content=content,posx=random.randrange(5,75),posy=random.randrange(5,30),speedy=1,speedx=0)
+objlist[2]=rgr.Graphic(width=4,height=4,posx=random.randrange(5,75),posy=random.randrange(5,30),speedy=1,speedx=0)
+fps=0
+n=0
+try:
+	while True:
+		fp.start()
+		objlist[1].move()
+		objlist[1].stayin(screen,1)
+		objlist[0].move()
+		objlist[0].stayin(objlist[1],1)
+		objlist[0].stayin(screen,1)
+		screen.draw(objlist[1])
+		screen.draw(objlist[0],offx=objlist[1].posx-2,offy=objlist[1].posy-1)
+		screen.display()
+		fps+=fp.end(counter=True)
+		n+=1
+except KeyboardInterrupt:
+	pass
+finally:
+	if n>0:
+		fps/=n
+	else:
+		fps=0
+	os.system("reset")
+	print("Average fps:",fps)
--- rgraphics.py
+++ rgraphics.py
@@ -18,10 +18,10 @@ if os.name=="nt":
 		os.system("cls")
 else:
 	def clearscreen():
-		os.system("clear")
-
+		os.system("reset")
+stdwrite=sys.stdout.write
 										#COLOR
-class color():
+class Color():
 	def __init__(self):
 		self.bck="\033[30m"
 		self.red="\033[31m"
@@ -32,128 +32,374 @@ class color():
 		self.cyn="\033[36m"
 		self.wht="\033[37m"
 		self.rst="\033[0m"
-colr=color()
+	def colrs(self,rst=False):
+		if rst:
+			return (self.bck,self.red,self.grn,self.yel,self.bue,self.mag,self.cyn,self.wht,self.rst)
+		else:
+			return (self.bck,self.red,self.grn,self.yel,self.bue,self.mag,self.cyn,self.wht)
+colr=Color()
+rst=colr.rst
 										#SHADES
-class shades():
+class MiscCont():
 	def __init__(self):
-		self.a="██"
-		self.b="▓▓"
-		self.c="▒▒"
-		self.d="░░"
-		self.e="  "
-shds=shades()
+		self.bar=		"㍴"
+		self.dm=		"㍷"
+		self.dm2=		"㍸"
+		self.dm3=		"㍹"
+		self.kb=		"㎅"
+		self.mb=		"㎆"
+		self.gb=		"㎇"
+		self.cal=		"㎈"
+		self.kcal=		"㎉"
+		self.hz=		"㎐"
+		self.khz=		"㎑"
+		self.mhz=		"㎒"
+		self.ghz=		"㎓"
+		self.cc=		"ᄄ"
+		self._2l=		"ᄘ"
+		self.dotb=		"⏺ "
+		self.dots=		"‧ "
+		self.b_circle=	"⚫"
+		self.h_circle=	"〇"
+		self.arr_left=	"〈"
+		self.arr_right=	"〉"
+		self.brack1_o=	"【"
+		self.brack1_c=	"】"
+		self.brack2_o=	"〔"
+		self.brack2_c=	"〕"
+		self.brack3_o=	"〖"
+		self.brack3_c=	"〗"
+		self.brack4_o=	"〘"
+		self.brack4_c=	"〙"
+		self.vase=		"〠"
+		self.refmark=	"※"
+		self.line=		"〡"
+		self.lines=		"||"
+		self.wiggx=		"〤"
+		self.wiggxx=	"〷"
+		self.cross=		"〸"
+		self.cross2=	"〹"
+		self.triangle=	"ᅀ"
+		self.pill=		"ↀ "
+		self.sonar=		"ↂ "
+		self.sonar2=	"ↈ "
+		self.anker=		"⚓"
+		self.check_mark="✅"
+		self.umbrella=	"☔"
+		self.coffee=	"☕"
+		self.stars=		"✨"
+		self.qmark_red=	"❓"
+		self.qmark_grey="❔"
+		self.emark_grey="❕"
+		self.emark_red=	"❗"
+		self.plus=		"➕"
+		self.minus=		"➖"
+		self.divison=	"➗"
+		self.m1=		"ᄀ"
+		self.m2=		"ᄁ"
+		self.m3=		"ᄂ"
+		self.m4=		"ᄇ"
+		self.m5=		"ᄈ"
+		self.m6=		"ᄉ"
+		self.m7=		"ᄒ"
+		self.m8=		"ᄓ"
+		self.m9=		"ᄖ"
+		self.m10=		"ᄗ"
+		self.m11=		"ᄘ"
+		self.m12=		"ᄠ"
+		self.m13=		"ᄡ"
+		self.m14=		"ᄢ"
+		self.m15=		"ᄣ"
+		self.m16=		"ᄤ"
+		self.m17=		"ᄥ"
+		self.m18=		"ᄦ"
+		self.m19=		"ᄧ"
+		self.m20=		"ᄨ"
+		self.m21=		"ᄩ"
+		self.m22=		"ᄰ"
+		self.m23=		"ᄱ"
+		self.m24=		"ᄲ"
+		self.m25=		"ᄳ"
+		self.m26=		"ᄴ"
+		self.m27=		"ᄵ"
+		self.m28=		"ᄶ"
+		self.m29=		"ᄷ"
+		self.m30=		"ᄸ"
+		self.m31=		"ᄹ"
+		self.m32=		"ᅁ"
+		self.m33=		"ᅂ"
+		self.m34=		"ᅃ"
+		self.m35=		"ᅄ"
+		self.m36=		"ᅅ"
+		self.m37=		"ᅆ"
+		self.m38=		"ᅈ"
+		self.m39=		"ᅉ"
+		self.m40=		"ᅐ"
+		self.m41=		"ᅑ"
+		self.m42=		"ᅒ"
+		self.m43=		"ᅓ"
+		self.m44=		"ᅔ"
+		self.m45=		"ᅕ"
+		self.m46=		"ᅖ"
+		self.m47=		"ᅗ"
+		self.m48=		"ᅘ"
+		self.m49=		"ᅙ"
+		self.m50=		"ឈ "
+		self.m51=		"ញ "
+		self.m52=		"យ "
+		self.m53=		"ℳ "
+		self.m54=		"ᄑ"
+		self.m55=		"〈"
+		self.m58=		"♈"
+		self.m59=		"♉"
+		self.m60=		"♐"
+		self.m61=		"♑"
+		self.m62=		"♒"
+		self.m63=		"♓"
+		self.m64=		"〄"
+	def shades(self):
+		return [value for name, value in vars(self).items()]
+class NumberCont():
+	def __init__(self):
+		self._0=	"ᄆ"
+		self._00=	"ᅇ"
+		self._2=	"ᄅ"
+		self._22=	"ᄙ"
+		self.r1=	"Ⅰ "
+		self.r2=	"Ⅱ "
+		self.r3=	"Ⅲ "
+		self.r4=	"Ⅳ "
+		self.r5=	"Ⅴ "
+		self.r6=	"Ⅵ "
+		self.r7=	"Ⅶ "
+		self.r8=	"Ⅷ "
+		self.r9=	"Ⅸ "
+		self.r10=	"Ⅹ "
+		self.c1=	"① "
+		self.c2=	"② "
+		self.c3=	"③ "
+		self.c4=	"④ "
+		self.c5=	"⑤ "
+		self.c6=	"⑥ "
+		self.c7=	"⑦ "
+		self.c8=	"⑧ "
+		self.c9=	"⑨ "
+		self.c10=	"⑩ "
+		self.c17=	"⑰ "
+		self.c18=	"⑱ "
+		self.c19=	"⑲ "
+		self.c20=	"⑳ "
+		self.b1=	"⑴ "
+		self.b2=	"⑵ "
+		self.b3=	"⑶ "
+		self.b4=	"⑷ "
+		self.b5=	"⑸ "
+		self.b6=	"⑹ "
+		self.b13=	"⒀ "
+		self.b14=	"⒁ "
+		self.b15=	"⒂ "
+		self.b16=	"⒃ "
+		self.b17=	"⒄ "
+		self.b18=	"⒅ "
+		self.b19=	"⒆ "
+		self.b20=	"⒇ "
+		self.p1=	"⒈ "
+		self.p1=	"⒉ "
+		self.p1=	"⒐ "
+		self.p1=	"⒑ "
+		self.p1=	"⒒ "
+		self.p1=	"⒓ "
+		self.p1=	"⒔ "
+		self.p1=	"⒕ "
+		self.p1=	"⒖ "
+		self.p1=	"⒗ "
+		self.p1=	"⒘ "
+		self.p1=	"⒙ "
+	def shades(self):
+		return [value for name, value in vars(self).items()]
+class LetterCont():
+	def __init__(self):
+		self.a=		"a "
+		self.b=		"b "
+		self.c=		"ᄃ"
+		self.d=		"d "
+		self.e=		"ᄐ"
+		self.f=		"f "
+		self.g=		"g "
+		self.h=		"h "
+		self.i=		"i "
+		self.j=		"j "
+		self.k=		"l "
+		self.l=		"ᄂ"
+		self.m=		"m "
+		self.o=		"o "
+		self.p=		"p "
+		self.q=		"q "
+		self.r=		"r "
+		self.s=		"s "
+		self.t=		"t "
+		self.u=		"u "
+		self.v=		"v "
+		self.w=		"w "
+		self.x=		"x "
+		self.y=		"y "
+		self.z=		"z "
+		self.ä=		"ä "
+		self.ö=		"ö "
+		self.ü=		"ü "
+		self.ll=	"ᄔ"
+		self.lc=	"ᄕ"
+	def shades(self):
+		return [value for name, value in vars(self).items()]
+class Shades():
+	def __init__(self):
+		self.a=	"██"
+		self.b=	"▓▓"
+		self.c=	"▒▒"
+		self.d=	"░░"
+		self.e=	"  "
+		self.let=LetterCont()
+		self.num=NumberCont()
+		self.misc=MiscCont()
+	def shades(self,shds=True,num=False,let=False,misc=False):
+		l=[]
+		if shds:
+			l+=[self.a,self.b,self.c,self.d,self.e]
+		if let:
+			l+=self.let.shades()
+		if num:
+			l+=self.num.shades()
+		if misc:
+			l+=self.misc.shades()
+		return l
+shds=Shades()
+class Settings():
+	def __init__(self):
+		self.warnings=True
+		self.spawnvisible=False
+		self.defwidth=2
+		self.defheight=2
+		self.defcontent=[]
+sett=Settings()
+										#PIXEL
+class Px():
+	def __init__(self,color=colr.bck,shade=shds.a):
+		self.shade=shade
+		self.color=color
+		if type(color)==type(shade)==type(""):
+			self.content=color+shade
+	def clear(self):
+		self.shade=shds.a
+		self.color=colr.bck
+	def equals(self,px):
+		if self.__class__==px.__class__:
+			return self.color==px.color and self.shade==px.shade
+		else:
+			return False
+
 										#GRAPHIC
-class graphic():
-	def __init__(self):
-		self.posx=0
-		self.posy=0
-		self.speedx=0
-		self.speedy=0
-		self.content=[]
-		self.bg=""
+class Graphic():
+	def __init__(self,visible=sett.spawnvisible,width=sett.defwidth,height=sett.defheight,bg=Px(),warning=sett.warnings,content=sett.defcontent,speedx=0,speedy=0,posx=1,posy=1):
+		if bg.__class__!=Px:
+			raise TypeError("Attribute 'bg' has to be a "+Px+", but it's a "+bg.__class__)
+		for row in content:
+			for col in row:
+				if col.__class__!=Px:
+					raise TypeError("Attribute 'content' has to be a list of lists of "+Px+", but it's a list od "+bg.__class__)
+		self.posx=posx
+		self.posy=posy
+		self.speedx=speedx
+		self.speedy=speedy
+		self.content=content
 		self.prevcontent=[]
-		self.hitbox=[]
-	def init(self,width,height,bg=shds.a,warning=True):
+		self.visible=visible
 		self.bg=bg
 		for row in range(height):
 			precontent=[]
 			nonearray=[]
 			for col in range(width):
 				precontent.append(self.bg)
-				nonearray.append(None)
-			self.content.append(precontent)
+				nonearray.append(Px(shade=None,color=None))
+			if self.content==sett.defcontent:
+				self.content.append(precontent)
 			self.prevcontent.append(nonearray)
-			self.hitbox.append(nonearray)
 		rows, columns=os.popen('stty size', 'r').read().split()
-		while int(rows)<height or int(columns)<width:
-			sys.stdout.write("You need an output that's "+str(width*3)+"x"+str(height)+" big, but it's only "+columns+"x"+rows+" big.\r")
-			sys.stdout.flush()
-			rows, columns=os.popen('stty size', 'r').read().split()
+		if int(rows)<height or int(columns)<width*2:
+			input("You need to zoom out! Press enter to continue.")
 	def display(self):
-		changes2write=[]
-		try:
-			bg=self.bg
-			for row in range(len(self.content)):
+		changes2write=""
+		bg=self.bg
+		for row in range(len(self.content)):
+			rowcontent=tuple(self.content[row])
+			prevrowcontent=tuple(self.prevcontent[row])
+			if rowcontent!=prevrowcontent:
 				rowstr=str(row+1)+";"
-				rowcontent=self.content[row]
+				waslastpixdiff=False		#this marks if the last checked pixel was different
+				waslastdrawdiffcolr=False	#this marks if the last checked pixel that will be drawn has the same color yes I know diff stands for different, but I'm too lazy to change the name now.
+				lastdrawcolr=None			#this is the color of the previousely drawn pixel
 				for col in range(len(rowcontent)):
-					if rowcontent[col]!=self.prevcontent[row][col]:
-						changes2write+=colr.rst+"\033["+rowstr+str((col*2)+1)+"H"+rowcontent[col]+colr.rst)
-						self.prevcontent[row][col]=rowcontent[col]
+					colcontent=rowcontent[col]
+					prevcolcontent=prevrowcontent[col]
+					if colcontent!=prevcolcontent:
+						waslastdrawdiffcolr=colcontent.color==lastdrawcolr
+						if waslastpixdiff and waslastdrawdiffcolr:
+							changes2write+=colcontent.content
+						elif waslastpixdiff:
+							changes2write+=rst+colcontent.content
+						elif waslastdrawdiffcolr:
+							changes2write+="\033["+rowstr+str((col*2)+1)+"H"+colcontent.content
+						else:
+							changes2write+=rst+"\033["+rowstr+str((col*2)+1)+"H"+colcontent.content
+						self.prevcontent[row][col]=colcontent
+						waslastpixdiff=True
+						lastdrawcolr=colcontent.color
+					else:
+						waslastpixdiff=False
 					self.content[row][col]=bg
-			sys.stdout.write(changes2write)
-			sys.stdout.flush()
-		except KeyboardInterrupt:
-			clearscreen()
-			quit()	
+		stdwrite("".join(changes2write))
 	def stayin(self,other,type=0):
-		try:
-			x=self.posx
-			y=self.posy
-			w=len(self.content)
-			h=len(self.content[0])
-			ow=len(other.content)
-			oh=len(other.content[0])
-			o=oh-h
-			o2=ow-w
-			if type==0:
-				if x<1:
-					self.posx=1
-				elif x>o:
-					self.posx=o
-				if y<1:
-					self.posy=1
-				elif y>o2:
-					self.posy=o2
-			elif type==1:
-				if x<1:
-					self.posx=2
-					self.speedx*=-1
-				elif x>o:
-					self.posx=o-1
-					self.speedx*=-1
-				if y<1:
-					self.posy=2
-					self.speedy*=-1
-				elif y>o2:
-					self.posy=o2-1
-					self.speedy*=-1
-			else:
-				raise ValueError("Wrong stayin type")
-		except KeyboardInterrupt:
-			clearscreen()
-			quit()
-	async def displayloop(self,maxfps=60):		
-		task = asyncio.Task(self.raw_displayloop(maxfps))
-		task.cancel()
-		with suppress(asyncio.CancelledError):
-			await task
-	async def raw_displayloop(self,maxfps):
-		f=fpslimiter()
-		f.maxfps=maxfps
-		while True:
-			f.start()
-			for item in self.drawpile:
-				self.draw(item)
-			self.display()
-			f.end()
-	def draw(other,self,noerr=False):
-		try:
-			posx=self.posx
-			precontent=other.content
-			for row in range(len(self.content)):
-				newrow=row+self.posy
-				rowcontent=self.content[row]
-				for col in range(len(rowcontent)):
-					if self.content[row][col] not in ["",None,'']:
-						precontent[newrow][col+posx]=rowcontent[col]
-			other.content=precontent
-		except KeyboardInterrupt:
-			clearscreen()
-			quit()
-		except:
-			if noerr==False:
-				raise ValueError("Failed to draw on object. self.position:"+str(self.posx)+"x"+str(self.posy)+", self.size: "+str(len(self.content))+"x"+str(len(self.content[0]))+", other.size: "+str(len(other.content))+"x"+str(len(other.content[0])))
+		x=self.posx
+		y=self.posy
+		w=len(self.content)
+		h=len(self.content[0])
+		ow=len(other.content)
+		oh=len(other.content[0])
+		o=oh-h
+		o2=ow-w
+		if type==0:		#Normal
+			if x<1:
+				self.posx=1
+			elif x>o:
+				self.posx=o
+			if y<1:
+				self.posy=1
+			elif y>o2:
+				self.posy=o2
+		elif type==1:	#Bounce
+			if x<1:
+				self.posx=2
+				self.speedx*=-1
+			elif x>o:
+				self.posx=o-1
+				self.speedx*=-1
+			if y<1:
+				self.posy=2
+				self.speedy*=-1
+			elif y>o2:
+				self.posy=o2-1
+				self.speedy*=-1
+		else:
+			raise ValueError("Wrong stayin type")
+	def draw(self,other,noerr=False,offx=0,offy=0):
+		posx=other.posx+offx
+		precontent=self.content
+		for row in range(len(other.content)):
+			newrow=row+other.posy+offy
+			rowcontent=other.content[row]
+			for col in range(len(rowcontent)):
+				if other.content[row][col] not in ("",None,''):
+					precontent[newrow][col+posx]=rowcontent[col]
+		self.content=precontent
 	def move(self):
 		try:
 			self.posx+=self.speedx
@@ -161,35 +407,24 @@ class graphic():
 		except KeyboardInterrupt:
 			clearscreen()
 			quit()
-	def collideswith(self,other):
-		collision=None
-		for x in range(len(self.hitbox)):
-			for y in range(len(other.hitbox)):
-				if self.posx+x==other.posx+y:
-					collision=False
-		if collision==None:
-			return False
-		else:
-			for x in range(len(self.hitbox[0])):
-				for y in range(len(other.hitbox[0])):
-					if self.posx+x==other.posx+y:
-						collision=True
-		return collision
-class fpslimiter():
+class FpsLimiter():
 	def __init__(self):
 		self.fps=0
 		self.maxfps=60
 		self.time=time.time()
 	def start(self):
 		self.time=time.time()
-	def end(self):
-		try:
-			mintime=1/self.maxfps
-			t=self.time+mintime
-			while time.time()<t:
-				pass
-			return 1/(time.time()-self.time)
-		except KeyboardInterrupt:
-			clearscreen()
-			quit()
-# ¢: ← If you need help, please torture this deamon.
+	def end(self,counter=False):
+		if counter:
+			return round(1/(time.time()-self.time))
+		else:
+			try:
+				mintime=1/self.maxfps
+				t=self.time+mintime
+				while time.time()<t:
+					pass
+				return round(1/(time.time()-self.time))
+			except KeyboardInterrupt:
+				clearscreen()
+				quit()
+# ¢: ← If you need help, please ask this daemon.
